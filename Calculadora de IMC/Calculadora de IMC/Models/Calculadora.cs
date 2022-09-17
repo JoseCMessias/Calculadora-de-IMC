bd467456ee5e33ee81e3ab55c9d50d7399a184f2
José Messias <104660308+JoseCMessias@users.noreply.github.com>
2022-09-17 19:25:56 -0300

    Atualizado

---
+++ Calculadora de IMC/Calculadora de IMC/Models/Calculadora.cs
@@ -0,0 +1,66 @@
+﻿using System.Globalization;
+
+namespace CalculadoraDeIMC.Models
+{
+    public class Calculadora
+    {
+        private double Peso { get;  set; }
+        private double Altura { get;  set; }
+
+        private double CalculoIMC()
+        {
+            return Peso / Math.Pow(Altura, 2);
+        }
+
+        private string Situacao(double imc)
+        {
+            // Váriavel
+            string retorno;
+
+            if (imc < 18.5)
+            {
+                retorno = "Abaixo do peso";
+            }
+            else if (imc < 25)
+            {
+                retorno = "Peso normal";
+            }
+            else if (imc < 30)
+            {
+                retorno = "A cima do peso";
+            }
+            else if (imc < 35)
+            {
+                retorno = "Obesidade I";
+            }
+            else if (imc < 40)
+            {
+                retorno = "Obesidade II";
+            }
+            else
+            {
+                retorno = "Obesidade III";
+            }
+            return retorno;
+        }
+
+        public void Mensagem()
+        {
+            Console.WriteLine("          ____________IMC____________");
+            Console.WriteLine();
+            Console.Write("          - Digite a sua altura: ");
+            Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("          - Digite o seu peso: ");
+            Peso = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            // Obter o cálculo
+            double obterCalculo = CalculoIMC();
+            //Obter Situação
+            string obterSituação = Situacao(obterCalculo);
+
+            Console.WriteLine($"          - Seu IMC é de {obterCalculo.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"          - Sua situação é de {obterSituação}");
+        }
+    }
+}
---
+++ Calculadora de IMC/Calculadora de IMC/Program.cs
@@ -0,0 +1,17 @@
+﻿using CalculadoraDeIMC.Models;
+
+namespace CalculadoraDeIMC 
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+
+            Calculadora calculadora = new Calculadora();   
+            calculadora.Mensagem();
+        }
+    }
+}
+
